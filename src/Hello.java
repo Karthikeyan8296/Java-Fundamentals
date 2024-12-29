d4d05511d1beaabdfed75d95ca7b505c6fed50f7
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2024-12-29 08:22:37 +0530

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,6 @@
+//just an testing class and obj
+public class Hello {
+     static void myMethod(){
+        System.out.println("vannkko");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //This is how we call class and function in java
+        Hello.myMethod();
+        System.out.println("is it working?");
+    }
+}
